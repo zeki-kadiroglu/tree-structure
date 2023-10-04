1cbcd99f8abf95e710bf955234cd151082f5bd04
zeki-kadiroglu <z.kdrglu07@gmail.com>
2023-10-04 18:00:50 +0300

    - zigzag search done

--- src/traverse/zigzag.py
+++ src/traverse/zigzag.py
@@ -1,19 +1,13 @@
 from collections import deque
+from src.nodeTree import TreeNode
+from src.traverse.base import BaseTree
 
-class Node:
-    def __init__(self, key):
-        self.val = key
-        self.child = []
-
-    def addChild(self, child_node):
-        self.child.append(child_node)
-
-class TreeNode:
-    def __init__(self, val=0, children=None):
-        self.val = val
+class ZigZag(BaseTree):
+    def __init__(self, payload=0, children=None):
+        self.payload = payload
         self.children = children if children is not None else []
 
-    def zigzagLevelOrder(self):
+    def iterate_traversal(self):
         if not self:
             return []
 
@@ -27,7 +21,7 @@ class TreeNode:
 
             for _ in range(level_size):
                 node = queue.popleft()
-                level.append(node.val)
+                level.append(node.payload)
 
                 for child in node.children:
                     queue.append(child)
@@ -46,48 +40,48 @@ if __name__ == "__main__":
     
     # Create a generic tree based on a given structure
     def create_tree():
-        root = Node(1)
-        child2 = Node(2)
-        child3 = Node(3)
-        root.addChild(child2)
-        root.addChild(child3)
+        root = TreeNode(1)
+        child2 = TreeNode(2)
+        child3 = TreeNode(3)
+        root.add_child(child2)
+        root.add_child(child3)
         
-        gchild1 = Node(4)
+        gchild1 = TreeNode(4)
         
-        child2.addChild(gchild1)
+        child2.add_child(gchild1)
         
-        child2.addChild(Node(5))
+        child2.add_child(TreeNode(5))
 
-        child3.addChild(Node(6))
-        child3.addChild(Node(7))
-        child3.addChild(Node(8))  # Adding a third child to node 3
+        child3.add_child(TreeNode(6))
+        child3.add_child(TreeNode(7))
+        child3.add_child(TreeNode(8))  # Adding a third child to node 3
 
-        # Add an additional level
-        child4 = Node(9)
-        child5 = Node(10)
+        # Add a level
+        child4 = TreeNode(9)
+        child5 = TreeNode(10)
         #child2.addChild(child4)
-        child2.addChild(child5)
+        child2.add_child(child5)
 
         # Add one more node
-        child6 = Node(11)
-        gchild1.addChild(child6)
-        gchild1.addChild(child4)
+        child6 = TreeNode(11)
+        gchild1.add_child(child6)
+        gchild1.add_child(child4)
 
         return root
 
-    # Convert to TreeNode for zigzagLevelOrder traversal
+    # Convert to ZigZag for zigzagLevelOrder traversal
     def convert_to_tree_node(node):
         if not node:
             return None
-        tn = TreeNode(node.val)
-        for child in node.child:
-            tn.children.append(convert_to_tree_node(child))
-        return tn
+        tree_node = ZigZag(node.payload)
+        for child in node.children:
+            tree_node.children.append(convert_to_tree_node(child))
+        return tree_node
 
     tree_root = convert_to_tree_node(create_tree())
 
     # Perform zigzag level order traversal
-    result = tree_root.zigzagLevelOrder()
+    result = tree_root.iterate_traversal()
 
     # Print the result
     for level in result:
