b40251d4c52699ef68e3a50b04df67296fcfb9f6
zeki-kadiroglu <z.kdrglu07@gmail.com>
2023-10-04 18:09:34 +0300

    - tests cases

---
+++ tests/test_breadth_first.py
@@ -0,0 +1,22 @@
+from src.nodeTree import TreeNode
+from src.traverse.breadth_first import BreadthFirst
+
+if __name__ == "__main__":
+    def create_sample_tree():
+        root = BreadthFirst("Root")
+        for i in range(3):
+            child1 = TreeNode("Child{}".format(i + 1))
+            for j in range(4):
+                child2 = TreeNode("Grandchild{}_{}".format(i + 1, j + 1))
+                child1.add_child(child2)
+                for k in range(2):
+                    child3 = TreeNode("GGC{}_{}_{}".format(i + 1, j + 1, k+1))
+                    child2.add_child(child3)
+
+            root.root.add_child(child1)
+        return root
+
+    tree = create_sample_tree()
+    print("\nBreadth-first traversal:")
+    for item in tree.iterate_traversal():
+        print(item)
---
+++ tests/test_deep_first.py
@@ -0,0 +1,21 @@
+from src.nodeTree import TreeNode
+from src.traverse.deep_first import DeepFirst
+
+if __name__ == "__main__":
+    def create_sample_tree():
+        root = DeepFirst("Root")
+        for i in range(3):
+            child1 = TreeNode("Child{}".format(i + 1))
+            for j in range(4):
+                child2 = TreeNode("Grandchild{}_{}".format(i + 1, j + 1))
+                child1.add_child(child2)
+                for k in range(2):
+                    child3 = TreeNode("GCTorun{}{}{}".format(i+1, j+1, k + 1))
+                    child1.add_child(child3)
+                root.root.add_child(child1)
+        return root
+    tree = create_sample_tree()
+
+    print("Depth-first traversal:")
+    for item in tree.iterate_traversal():
+        print(item)
