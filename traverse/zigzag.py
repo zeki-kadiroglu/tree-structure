ae71b39711875c872120fe06450c9c522dc3d36c
zeki-kadiroglu <67226258+zeki-kadiroglu@users.noreply.github.com>
2023-10-04 11:12:32 +0000

    zigzag tree modified

--- traverse/zigzag.py
+++ traverse/zigzag.py
@@ -1,35 +1,94 @@
+from collections import deque
+
+class Node:
+    def __init__(self, key):
+        self.val = key
+        self.child = []
+
+    def addChild(self, child_node):
+        self.child.append(child_node)
+
 class TreeNode:
-    def __init__(self, value):
-        self.value = value
-        self.children = []
-    def add_child(self, child):
-        self.children.append(child)
+    def __init__(self, val=0, children=None):
+        self.val = val
+        self.children = children if children is not None else []
 
-class Tree:
-    def __init__(self, root_payload):
-        self.root = TreeNode(root_payload)
+    def zigzagLevelOrder(self):
+        if not self:
+            return []
 
-    def create_tree(self, root_value, depth, branching_factor):
-        if depth == 0:
-            return Tree(root_value)
+        result = []
+        queue = deque([self])
+        reverse_order = False
 
-        root = TreeNode(root_value)
-        for i in range(0, branching_factor+1):
-            child_value = root_value + str(i) + "1"
-            root.add_child(self.create_tree(child_value, depth - 1, branching_factor))
+        while queue:
+            level = []
+            level_size = len(queue)
+
+            for _ in range(level_size):
+                node = queue.popleft()
+                level.append(node.val)
+
+                for child in node.children:
+                    queue.append(child)
+
+            if reverse_order:
+                level = level[::-1]
+
+            result.append(level)
+            reverse_order = not reverse_order
+
+        return result
+
+
+
+if __name__ == "__main__":
+    
+    # Create a generic tree based on a given structure
+    def create_tree():
+        root = Node(1)
+        child2 = Node(2)
+        child3 = Node(3)
+        root.addChild(child2)
+        root.addChild(child3)
+        
+        gchild1 = Node(4)
+        
+        child2.addChild(gchild1)
+        
+        child2.addChild(Node(5))
+
+        child3.addChild(Node(6))
+        child3.addChild(Node(7))
+        child3.addChild(Node(8))  # Adding a third child to node 3
+
+        # Add an additional level
+        child4 = Node(9)
+        child5 = Node(10)
+        #child2.addChild(child4)
+        child2.addChild(child5)
+
+        # Add one more node
+        child6 = Node(11)
+        gchild1.addChild(child6)
+        gchild1.addChild(child4)
 
         return root
 
-# Helper function to print the tree structure
-def print_tree(node, level=1):
-    if node is not None:
-        print("  " * level + str(node.value))
-        for child in node.children:
-            print_tree(child, level + 1)
+    # Convert to TreeNode for zigzagLevelOrder traversal
+    def convert_to_tree_node(node):
+        if not node:
+            return None
+        tn = TreeNode(node.val)
+        for child in node.child:
+            tn.children.append(convert_to_tree_node(child))
+        return tn
 
-# Example usage:
-t = TreeNode("root")
-root = t.create_tree("root", 4 ,3)
-# root = create_tree("root", 4, 3)
-print("Tree Structure:")
-print_tree(root)
+    tree_root = convert_to_tree_node(create_tree())
+
+    # Perform zigzag level order traversal
+    result = tree_root.zigzagLevelOrder()
+
+    # Print the result
+    for level in result:
+        print(level)
